c964a019ee9c7b79efaa69db11d2ad7533dc325a
George Song <george@hiailabs.com>
2024-07-05 12:48:04 -0700

    feat: quick and dirty homepage

--- src/navigation.js
+++ src/navigation.js
@@ -61,27 +61,27 @@ export const headerData = {
       links: [
         {
           text: "Lead Generation",
-          href: getPermalink("/landing/lead-generation"),
+          href: getPermalink("/example/landing/lead-generation"),
         },
         {
           text: "Long-form Sales",
-          href: getPermalink("/landing/sales"),
+          href: getPermalink("/example/landing/sales"),
         },
         {
           text: "Click-Through",
-          href: getPermalink("/landing/click-through"),
+          href: getPermalink("/example/landing/click-through"),
         },
         {
           text: "Product Details (or Services)",
-          href: getPermalink("/landing/product"),
+          href: getPermalink("/example/landing/product"),
         },
         {
           text: "Coming Soon or Pre-Launch",
-          href: getPermalink("/landing/pre-launch"),
+          href: getPermalink("/example/landing/pre-launch"),
         },
         {
           text: "Subscription",
-          href: getPermalink("/landing/subscription"),
+          href: getPermalink("/example/landing/subscription"),
         },
       ],
     },
@@ -130,25 +130,10 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: "Product",
-      links: [
-        { text: "Features", href: "#" },
-        { text: "Security", href: "#" },
-        { text: "Team", href: "#" },
-        { text: "Enterprise", href: "#" },
-        { text: "Customer stories", href: "#" },
-        { text: "Pricing", href: "#" },
-        { text: "Resources", href: "#" },
-      ],
-    },
-    {
       title: "Platform",
       links: [
         { text: "Developer API", href: "#" },
         { text: "Partners", href: "#" },
-        { text: "Atom", href: "#" },
-        { text: "Electron", href: "#" },
-        { text: "AstroWind Desktop", href: "#" },
       ],
     },
     {
@@ -156,21 +141,14 @@ export const footerData = {
       links: [
         { text: "Docs", href: "#" },
         { text: "Community Forum", href: "#" },
-        { text: "Professional Services", href: "#" },
-        { text: "Skills", href: "#" },
-        { text: "Status", href: "#" },
       ],
     },
     {
       title: "Company",
       links: [
         { text: "About", href: "#" },
-        { text: "Blog", href: "#" },
         { text: "Careers", href: "#" },
         { text: "Press", href: "#" },
-        { text: "Inclusion", href: "#" },
-        { text: "Social Impact", href: "#" },
-        { text: "Shop", href: "#" },
       ],
     },
   ],
@@ -180,17 +158,10 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: "X", icon: "tabler:brand-x", href: "#" },
-    { ariaLabel: "Instagram", icon: "tabler:brand-instagram", href: "#" },
-    { ariaLabel: "Facebook", icon: "tabler:brand-facebook", href: "#" },
-    { ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },
     {
       ariaLabel: "Github",
       icon: "tabler:brand-github",
-      href: "https://github.com/onwidget/astrowind",
+      href: "https://github.com/orgs/hiai-labs",
     },
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
 };
